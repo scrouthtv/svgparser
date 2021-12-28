a8329f62e0f562a3cbc14342091927f581313847
Lenni vH <lennivh24@gmail.com>
2021-12-28 01:02:47 +0100

    encode element

---
+++ encode.go
@@ -0,0 +1,45 @@
+package svgparser
+
+import (
+	"encoding/xml"
+	"io"
+)
+
+func (e *Element) Encode(w io.Writer) error {
+	enc := xml.NewEncoder(w)
+
+	err := e.write(enc)
+	if err != nil {
+		return err
+	}
+
+	return enc.Flush()
+}
+
+func (e *Element) write(enc *xml.Encoder) error {
+	err := enc.EncodeToken(xml.StartElement{
+		Name: e.Name,
+		Attr: e.Attributes,
+	})
+	if err != nil {
+		return err
+	}
+
+	if e.Content != nil {
+		err = enc.EncodeToken(xml.CharData([]byte(e.Content)))
+		if err != nil {
+			return err
+		}
+	} else {
+		for _, c := range e.Children {
+			err = c.write(enc)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return e.EncodeToken(xml.EndElement{
+		Name: e.Name,
+	})
+}
